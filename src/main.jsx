0c84392a4099f960973b06e8e340ab410bd821ac
salma607 <salma25102001@gmail.com>
2025-05-14 13:40:53 +0300

    error

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,10 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./redux/Store";
+import store from "./redux/Store.js";
+
+
+
 
 
 
